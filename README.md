d4fb0a01cee2af40cf1c46af62e48baa9490de0e
Allison Piovani <apiovani25@gmail.com>
2020-10-15 19:06:57 -0300

    Configs initials of app

--- README.md
+++ README.md
@@ -16,3 +16,15 @@ docker volume create --name=nerau_cx_crud_laravel-database
 ```
 docker-compose up -d
 ```
+
+
+# Configurações Iniciais da Aplicação
+
+1 .
+Na pasta da aplicação. Copie o arquivo ".env.example" criando um novo arquivo chamado ".env'
+
+2 .
+Execute o seguinte comando:
+```
+php artisan migrate
+```
--- database/migrations/2020_10_15_215052_create_clients_table.php
+++ database/migrations/2020_10_15_215052_create_clients_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateClientsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,13 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('clients', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -27,6 +30,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('clients');
     }
 }
